5d008d88ace2755f5001171b0fa7ee185795da2e
C15417202 <62797637+C15417202@users.noreply.github.com>
2020-04-06 15:54:18 +0100

    Update hello.py

--- DisplayScreen/hello.py
+++ DisplayScreen/hello.py
@@ -1,7 +1,7 @@
 #
 # This Python Code, Recieves CAN Bus Data and Accelerometer Data and Displays them on the Drivers Screen.
 # By: Raimonds Kaminskis
-# Last Updated, 25/03/2020
+# Last Updated, 06/04/2020
 #
 
 # Importing required libraries
@@ -12,6 +12,7 @@ import sys
 import can
 import socket
 import struct
+import MySQLdb as mdb
 
 from PySide2.QtWidgets import QApplication
 from PySide2.QtUiTools import QUiLoader
@@ -30,13 +31,13 @@ def ButtonHandler():
     print("Got a button press")
     bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
     msg = can.Message(arbitration_id = 0x10, is_fd = 0, data=[0x34,0x12])
-    print(msg)
     bus.send(msg)
+    print("CAN Message Sent")
 
 def refreshScreenFunction():
-    # Initialize I2C bus.
+    #Initialize I2C bus
     i2c = busio.I2C(board.SCL, board.SDA)
-    # Initialize MMA8451 module.
+    #Initialize MMA8451 module.
     sensor = adafruit_mma8451.MMA8451(i2c)
     x, y, z = sensor.acceleration
     accelerationString = "Acceleration(m/s^2): x= " +str('{0:.3f}'.format(x)) + " y= " +str('{0:.3f}'.format(y))+ " z= " +str('{0:.3f}'.format(z))
@@ -45,6 +46,13 @@ def refreshScreenFunction():
     can_id,can_dlc,data = struct.unpack(can_fmt,cf)
     can_id = can_id & 0x7fffffff
     CANString = "ID: "+str(can_id) + " len: "+str(can_dlc)+ " data: "+ str(data)
+
+    con = mdb.connect('localhost', 'root', '12345678', 'pyqt5')
+    cur = con.cursor()
+    cur.execute("INSERT INTO data(CAN_ID, CAN_LEN, CAN_DATA) VALUES(%d, %d, %d) " %(can_id, can_dlc, data))
+    con.commit()
+    print("Data Inserted Successfully")
+
     if(str(can_id) == "1"):
         if(str(data) == "1"):
             MainWindow.eStop.setText("Pressed")
@@ -54,19 +62,26 @@ def refreshScreenFunction():
         MainWindow.bmsTemp_1.setText(str(data))
         MainWindow.bmsTemp_2.setText(str(data))
     if(str(can_id) == "3"):
-        MainWindow.speed_1.display(str(data))
-        MainWindow.speed_2.setText(str(data))
+        MainWindow.speed.display(str(data))
     if(str(can_id) == "4"):
         MainWindow.bmsVoltage_1.setValue(data)
         MainWindow.bmsVoltage_2.setText(str(data))
     if(str(can_id) == "5"):
         MainWindow.bmsCurrent.setText(str(data))
     if(str(can_id) == "6"):
-        MainWindow.motorVoltage.setText(str(data))
-    if(str(can_id) == "7"):
-        MainWindow.motorCurrent.setText(str(data))
-    if(str(can_id) == "8"):
         MainWindow.tsPower.setText(str(data))
+    if(str(can_id) == "7"):
+        MainWindow.motorVoltageLeft.setText(str(data))
+    if(str(can_id) == "8"):
+        MainWindow.motorVoltageRight.setText(str(data))
+    if(str(can_id) == "9"):
+        MainWindow.motorCurrentLeft.setText(str(data))
+    if(str(can_id) == "10"):
+        MainWindow.motorCurrentRight.setText(str(data))
+    if(str(can_id) == "11"):
+        MainWindow.steering.setText(str(data))
+    if(str(can_id) == "12"):
+        MainWindow.throttle.setText(str(data))
     MainWindow.Rmessages.append(CANString)
     MainWindow.Rmessages_2.append(accelerationString)
     QApplication.processEvents()
